69521be5e3271c376073c2b630b18e0a749d4fa0
kxshiii <brian.gikaru@student.moringaschool.com>
2025-05-07 14:23:36 +0300

    comm

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([]) 
+    elif length == 1:
+        print([0])  
+    else:
+        fibonacci = [0, 1]
+        for i in range(2, length):
+            fibonacci.append(fibonacci[i - 1] + fibonacci[i - 2])
+        print(fibonacci) 
